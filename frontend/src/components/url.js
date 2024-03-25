43e329f711d85726c749ee2045c2b36a1039b177
NavaprettamN <navaprettam.n214@gmail.com>
2024-03-26 00:41:27 +0530

    // -> /

--- frontend/src/components/url.js
+++ frontend/src/components/url.js
@@ -1,2 +1,2 @@
-export const URL = "https://prolog-2-navaprettamns-projects.vercel.app/"
+export const URL = "https://prolog-2-navaprettamns-projects.vercel.app"
 export const local_url = "http://localhost:5000"
